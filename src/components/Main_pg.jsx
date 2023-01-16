8bd611348ffd226ffecde4798d2c6388c271dc51
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-01-16 22:48:31 +0530

    Design completed

--- src/components/Main_pg.jsx
+++ src/components/Main_pg.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Quiz from './Quiz';
+import { data } from './Constants';
 
 function Main_pg() {
+  const [timeOut, setTimeOut] = useState(false);
   return (
     <>
       <div className='main-pg'>
         <div className="top">
           <div className="timer">50</div>
         </div>
-        <div className="bottom">QndA</div>
+        <div className="bottom">
+          <Quiz data={data} setTimeOut={setTimeOut}/>
+        </div>
       </div>
     </>
   )
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Quiz() {
+  return (
+    <>
+        <div className="quiz-section">
+            <div className="question">Question</div>
+            <div className="answers">
+                <div className="answer ">ans1</div>
+                <div className="answer ">ans2</div>
+                <div className="answer ">ans3</div>
+                <div className="answer ">ans4</div>
+            </div>
+        </div> 
+    </>
+  )
+};
+
+export default Quiz;
