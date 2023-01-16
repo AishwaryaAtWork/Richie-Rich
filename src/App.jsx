a40a1cdaf3e4676e906221faabbdb408a3361894
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-01-16 21:05:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Main_pg from "./components/Main_pg";
+import Money_pyramid from "./components/Money_pyramid";
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Main_pg/>
+      <Money_pyramid/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main_pg.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Main_pg() {
+  return (
+    <>
+      <div className='main-pg'>
+        <div className="top">
+          <div className="timer">50</div>
+        </div>
+        <div className="bottom">QndA</div>
+      </div>
+    </>
+  )
+};
+
+export default Main_pg;
